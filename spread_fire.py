462c22ab95c24e94abc6826de2a47c0d0597803e
Layan2005317 <layan.khamisa@gmail.com>
2025-01-19 13:35:42 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -1,16 +1,29 @@
 import copy
 
+
 def spread_fire(grid):
-    new_grid = grid.copy()
-    for i in range(grid.shape[0]):
-        for j in range(grid.shape[1]):
-            if grid[i, j] == RED:
-                # Spread to neighbouring orange cells
-                for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
-                    if 0 <= x < grid.shape[0] and 0 <= y < grid.shape[1]:  # Boundary check
-                        if grid[x, y] == ORANGE:
-                            new_grid[x, y] = RED
-    return new_grid
+    """Update the forest grid based on fire spreading rules."""
+    grid_size = len(grid)
+    update_grid = copy.deepcopy(grid)
+    
+    for i in range(grid_size):  # Iterate over all rows
+        for j in range(grid_size):  # Iterate over all columns
+            if grid[i][j] == 1:  # If the cell contains a tree
+                neighbors = []
+                
+                if i > 0:  # Check above
+                    neighbors.append(grid[i-1][j])
+                if i < grid_size - 1:  # Check below
+                    neighbors.append(grid[i+1][j])
+                if j > 0:  # Check left
+                    neighbors.append(grid[i][j-1])
+                if j < grid_size - 1:  # Check right
+                    neighbors.append(grid[i][j+1])
+
+                if 2 in neighbors:
+                    update_grid[i][j] = 2
+
+    return update_grid
 
 
 
